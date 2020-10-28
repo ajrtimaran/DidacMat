02d3650fdb74e7e0a37e46446cd558345fcceac6
Carlos Timarán <ctimaran@iemsantateresita.edu.co>
2020-10-28 11:34:33 -0500

    Agregado contador de tiempo

--- app/src/main/java/com/example/didacmat/Suma.java
+++ app/src/main/java/com/example/didacmat/Suma.java
@@ -4,15 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Suma extends AppCompatActivity {
 
     ImageView numero1;
     ImageView numero2;
     Button boton1, boton2, boton3;
+
+    TextView reloj, puntos;
+
+
+    private CountDownTimer timer;
+    private int tiempo, segundos;
+
+    private int suma;
+    private int puntuacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +36,37 @@ public class Suma extends AppCompatActivity {
         boton2 = findViewById(R.id.buttonr2);
         boton3 = findViewById(R.id.buttonr3);
 
+        reloj = findViewById(R.id.textViewTiempo);
+        puntos = findViewById(R.id.textViewPuntos);
+
+        segundos = 20;
+        iniciarJuego();
         nuevaSuma();
+    }
+
+    public void iniciarJuego(){
+        tiempo = segundos;
+        puntuacion = 0;
+        reloj.setText(Integer.toString(tiempo));
+        timer = new CountDownTimer(segundos*1000,1000 ) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tiempo -= 1;
+                reloj.setText(Integer.toString(tiempo));
+            }
+
+            @Override
+            public void onFinish() {
+                 finalizarJuego();
+            }
+        };
+
+        timer.start();
+    }
+
+    public void finalizarJuego(){
+        numero1.setVisibility(View.INVISIBLE);
+        numero2.setVisibility(View.INVISIBLE);
     }
 
     public void cargarNumero(int numero, ImageView image){
@@ -64,7 +105,15 @@ public class Suma extends AppCompatActivity {
     }
 
     public void evaluaRespuesta(View v){
+        Button b = (Button) v;
+        int respuesta = Integer.parseInt(b.getText().toString());
 
+        if (respuesta == suma){
+            puntuacion += 1;
+
+
+        }
+        puntos.setText(Integer.toString(puntuacion));
         nuevaSuma();
     }
 
@@ -76,10 +125,10 @@ public class Suma extends AppCompatActivity {
         cargarNumero(n1, numero1);
         cargarNumero(n2, numero2);
 
-        int res = n1+n2;
+        suma = n1+n2;
 
-        int v1 = Math.abs(res - ((int) (Math.random() * n1) + 1));
-        int v2 = Math.abs(res + ((int) (Math.random() * n1) + 1));
+        int v1 = Math.abs(suma - ((int) (Math.random() * n1) + 1));
+        int v2 = Math.abs(suma + ((int) (Math.random() * n1) + 1));
         int v3 = Math.abs(v1 - ((int) (Math.random() * n2) + 1));
 
 
@@ -89,15 +138,13 @@ public class Suma extends AppCompatActivity {
 
 
 
-        if (posicion == 0) v1 = res;
+        if (posicion == 0) v1 = suma;
         else{
-            if (posicion == 1) v2 = res;
-            else v3 = res;
+            if (posicion == 1) v2 = suma;
+            else v3 = suma;
         }
-
         boton1.setText(Integer.toString(v1));
         boton2.setText(Integer.toString(v2));
         boton3.setText(Integer.toString(v3));
     }
-
 }
